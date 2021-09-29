ec943d05a359cf3cedd53355c29339ad84ae9f5a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-29 10:31:56 +0300

    init

--- DebbugingSwiftUIUsingSelfprintChanges/ContentView.swift
+++ DebbugingSwiftUIUsingSelfprintChanges/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isOn: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        let _ = Self._printChanges()
+        
+        VStack {
+            Image(systemName: isOn ? "lightbulb.fill" : "lightbulb")
+                .font(.system(size: 50))
+            
+            Toggle(isOn: $isOn) {
+                EmptyView()
+            }.fixedSize()
+        }
     }
 }
 
