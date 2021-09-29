675812ef121202cc7ba6997f9671b6435cdc977f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-29 10:25:42 +0300

    Initial Commit

---
+++ DebbugingSwiftUIUsingSelfprintChanges/DebbugingSwiftUIUsingSelfprintChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebbugingSwiftUIUsingSelfprintChangesApp.swift
+//  DebbugingSwiftUIUsingSelfprintChanges
+//
+//  Created by ramil on 29.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct DebbugingSwiftUIUsingSelfprintChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
